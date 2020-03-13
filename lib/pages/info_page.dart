bbc027e4c6e2a1a53850eea07915dc760a91d847
AdiKK17 <kavikmr9@gmail.com>
2020-03-13 20:25:47 +0530

    Updated

---
+++ lib/pages/info_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class InfoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("About the App"),
+      ),
+      body: Center(child: Text("Yes, it does contain errors!!!"),),
+    );
+  }
+}
